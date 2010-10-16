2d4a94de0be6c3aa34f79597225ed4b5609b9f7b
Aleksandr Koss <kossnocorp@gmail.com>
2010-10-16 07:31:30 +0700

    Hello cruel world!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sparta::Application.config.secret_token = 'b999434c45cef9b69e732089ed0ea88c20782ed1e81b60db8e76e3b56143dac3682da3999133aa5bbaa194de895c0de44a3b5ad2628c16329b71f26813646bde'
