9bc0bdf7612ec4c5d68e316262c77b661f2b909e
Edward Tsech <edtsech@gmail.com>
2010-10-17 00:57:46 +0700

    Create list of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,13 @@
 class CommentsController < InheritedResources::Base
-  actions
+  actions :index, :create
   nested_belongs_to :project, :ticket
+  
+  def index
+    super do |format|
+      @comment = Comment.new
+      format.html do
+        render :partial => 'list', :layout => false
+      end
+    end        
+  end
 end
