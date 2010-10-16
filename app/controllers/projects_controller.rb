75659f1e987dd2fbece4827d1c31ac8707a356ac
Edward Tsech <edtsech@gmail.com>
2010-10-16 11:49:29 +0700

    Create link from project to tickets#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,14 @@
 class ProjectsController < InheritedResources::Base
+  actions :index, :new, :create, :edit, :update, :destroy
+  
   def create
     create!
     current_user.projects << @project
+  end
+  
+  private
+  
+  def begin_of_association_chain
+    @current_user
   end
 end
