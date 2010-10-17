6c8f398b49387a64f59499220e1ca493d650d9d2
Aleksandr Koss <kossnocorp@gmail.com>
2010-10-17 15:56:29 +0700

    Added jsscrollpane plugin (with friends)

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -125,7 +125,7 @@ $(function() {
     selected: function(event, ui) {
       var selected = $('.ticket.ui-selected');
       if(selected.length > 1) {
-        $('.columns .column.meta-column').html('Goo');
+        metaPanel.html('Goo');
       } else if (selected.length === 1) {
         $.get($('span.number a', selected).attr('href'), function(html){
           metaPanel.html(html);
