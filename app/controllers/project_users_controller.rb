b620f39dbf6b8c044a24ee15c89192cdff6bc95d
Edward Tsech <edtsech@gmail.com>
2010-10-18 05:25:25 +0700

    Add user to project

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -2,17 +2,26 @@ class ProjectUsersController < InheritedResources::Base
   defaults :resource_class => User, :collection_name => 'users', :instance_name => 'user'
 
   belongs_to :project
-  actions :index, :destroy
+  actions :index, :create, :destroy
 
   respond_to :js
+  
+  def index
+    @project = Project.find(params[:project_id])
+    @user = @project.users.new
+    index!
+  end
 
-  def create
+  def create        
+    @project = Project.find(params[:project_id])
+    @project.users << User.find_by_email(params[:user][:email])
+    @user = @project.users.last
     create! { |format| format.js { render :partial => 'user', :locals => { :user => @user } } }
   end
 
   def destroy
     @project = Project.find(params[:project_id])
     @project.users.delete User.find(params[:id])
-    redirect_to :action => :index
+    redirect_to :controller => 'tickets'
   end
 end
