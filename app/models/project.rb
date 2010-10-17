8927c7bd6072f1711f085c9ae28d399ec1655da3
Edward Tsech <edtsech@gmail.com>
2010-10-17 11:45:38 +0700

    Create functionality for adding users to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,14 @@ class Project < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :tickets
   
-  def user    
+  def project_users
+    users
   end
   
+  def user
+    
+  end
+    
   def user=(email)
     self.users << User.where('email = ?', email)
   end
