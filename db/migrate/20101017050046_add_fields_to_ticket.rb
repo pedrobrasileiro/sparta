d113654bb4a9010e7da4688d0ecd054bac08c15a
Edward Tsech <edtsech@gmail.com>
2010-10-17 12:09:59 +0700

    Small changes

--- db/migrate/20101017050046_add_fields_to_ticket.rb
+++ db/migrate/20101017050046_add_fields_to_ticket.rb
@@ -1,11 +1,11 @@
 class AddFieldsToTicket < ActiveRecord::Migration
   def self.up
-    add_column :ticket, :reporter_id, :integer
-    add_column :ticket, :assigned_to_id, :integer
+    add_column :tickets, :reporter_id, :integer
+    add_column :tickets, :assigned_to_id, :integer
   end
 
   def self.down
-    remove_column :ticket, :reporter_id
-    remove_column :ticket, :assigned_to_id
+    remove_column :tickets, :reporter_id
+    remove_column :tickets, :assigned_to_id
   end
 end
