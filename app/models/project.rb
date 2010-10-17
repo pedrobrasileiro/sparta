17071a04b636992deca5dd33168d20e23f8745ea
Edward Tsech <edtsech@gmail.com>
2010-10-17 10:12:01 +0700

    Create adding user to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,11 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :tickets
+  
+  def user    
+  end
+  
+  def user=(email)
+    self.users << User.where('email = ?', email)
+  end
 end
