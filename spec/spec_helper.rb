3767769e38170bb11a858492c3064c09c895d42d
Aleksandr Koss <kossnocorp@gmail.com>
2010-10-23 15:41:12 +0700

    Added database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec/rails'
 require 'factory_girl'
 require 'faker'
 require 'cancan/matchers'
+require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -19,6 +20,16 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
   config.mock_with :rspec
+
+  DatabaseCleaner.strategy = :transaction
+
+  config.before :suite do
+  end
+
+  config.after :suite do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   #config.fixture_path = "#{::Rails.root}/spec/fixtures"
